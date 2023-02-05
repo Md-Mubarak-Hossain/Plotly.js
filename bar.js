cd2e374662ef89f2a21c503a2af8e2bcf69c8770
MD MUBARAK HOSSAIN <hossainmubarak477@gmail.com>
2023-02-06 02:04:14 +0600

    javascript plot library use in scatter bar lin chart

---
+++ bar.js
@@ -0,0 +1,20 @@
+// scatter plot
+let xArray=[50,60,70,80,90,100,110,120,130,140,150]
+let yArray=[7,8,14,9,9,9,10,11,14,14,15]
+
+// Define data
+let data=[{
+    x:xArray,
+    y:yArray,
+    mode:'markers',
+    type:'bar'
+}]
+
+// define layout
+let layout={
+    xaxis:{range:[40,160],title:'Square meters'},
+    yaxis:{range:[5,16],title:'Price is Millions'},
+    title:"House Price vs size"
+};
+// display using plotly
+Plotly.newPlot("myPlotBar",data,layout);
